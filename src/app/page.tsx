d9db4bf55fb6e9737a2871891c44730391ea0e82
siddanth-6365 <siddanth6365@gmail.com>
2024-01-10 23:08:11 +0530

    installations done

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-center">
+
+        <Button>rwerwerwer </Button>
+      </div>
+
+    </>
+  )
+}
